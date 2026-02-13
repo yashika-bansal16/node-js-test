55622eb56e45961706103a3b234cbafe76a08da1
yashika bansal <yashikabansal1616@gmail.com>
2026-02-13 11:50:23 +0530

    commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// const http = require('http');
+
+//  const myServer = http.createServer((req,res)=>{
+//      if(req.url == '/'){
+//          res.end('ABES Engineering College');
+//     }
+//     else if(req.url == '/about'){
+//          res.writeHead(200,{'Content-Type':'text/html'});
+//          res.end('<img src="https://college4u.in/wp-content/uploads/2018/03/abes.png"></img>');
+//      }
+//      else if(req.url == '/contact'){
+//          res.end('1234567890');
+//     }
+//     else{
+//         res.end('404 Page Not Found');
+//     }
+// })
+
+//  myServer.listen(8000,() => console.log('Server is running'));
+const fs = require('fs');
+fs.writeFile('myfile.txt','Hello World',(err)=>{
+    if(err) throw err;
+    console.log('File created successfully');
+});
