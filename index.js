668ed92894c0f59ec6851a45bac37eebcaa8b25b
yashika bansal <yashikabansal1616@gmail.com>
2026-02-13 12:18:22 +0530

    3rd commit

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
-// const http = require('http');
 
-//  const myServer = http.createServer((req,res)=>{
-//      if(req.url == '/'){
-//          res.end('ABES Engineering College');
-//     }
-//     else if(req.url == '/about'){
-//          res.writeHead(200,{'Content-Type':'text/html'});
-//          res.end('<img src="https://college4u.in/wp-content/uploads/2018/03/abes.png"></img>');
-//      }
-//      else if(req.url == '/contact'){
-//          res.end('1234567890');
-//     }
-//     else{
-//         res.end('404 Page Not Found');
-//     }
-// })
-
-//  myServer.listen(8000,() => console.log('Server is running'));
+const http = require('http');
 const fs = require('fs');
+
+const myServer = http.createServer((req, res) => {
+    res.end("Hello from server");
+});
+
+myServer.listen(8000, () => {
+    console.log('Server is running');
+});
+
 fs.writeFile('myfile.txt','Hello World',(err)=>{
     if(err) throw err;
     console.log('File created successfully');
@@ -27,14 +19,21 @@ fs.writeFile('myfile.txt','Hello Node.js',(err)=>{
     if(err) throw err;
     console.log('File updated successfully');
 });
+
 fs.readFile('myfile.txt','utf-8',(err,data)=>{
     if(err) throw err;
-    else{
-        console.log('File read successfully');
-    }
+    console.log('File read successfully');
     console.log(data);
 });
+
 fs.appendFile('myfile.txt',' Welcome to Node.js',(err)=>{
     if(err) throw err;
     console.log('File appended successfully');
+});
+
+fs.cpSync('myfile.txt','myfile_copy.txt');
+
+fs.rename('myfile_copy.txt','myfile_renamed.txt',(err)=>{
+    if(err) throw err;
+    console.log('File renamed successfully');
 });
